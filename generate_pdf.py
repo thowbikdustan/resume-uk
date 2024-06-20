00f5ef3f14375c21cd15041a875a05abfb53617a
Thowbik Dustan <beejascaptain@gmail.com>
2024-06-20 09:30:41 +0100

    Adding auto startup server config to python script

--- generate_pdf.py
+++ generate_pdf.py
@@ -1,5 +1,31 @@
 from pyhtml2pdf import converter
 import pdfkit
 
-# converter.convert('localhost:8000', 'resume.pdf')
-pdfkit.from_url('http://localhost:8000', 'resume.pdf')
+import http.server
+import socketserver
+
+import threading
+import time
+
+PORT = 8000
+Handler = http.server.SimpleHTTPRequestHandler
+
+def server():
+    with socketserver.TCPServer(("", PORT), Handler) as httpd:
+        print(f"Serving on Port: {PORT}")
+        httpd.serve_forever()
+
+def save_as_pdf():
+    pdfkit.from_url('http://localhost:8000', 'resume1.pdf')
+
+if __name__ == "__main__":
+    
+    # Starting seperate thread for running the server
+    server_thread = threading.Thread(target=server)
+    server_thread.daemon = True
+    server_thread.start()
+
+    # Server startup delay
+    time.sleep(2)
+
+    save_as_pdf()
